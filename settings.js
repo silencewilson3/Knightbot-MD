2608428d5dcaf9a3cdb9907cb01655e10e3aa23b
silencewilson3 <steeps-55.may@icloud.com>
2025-07-09 18:49:04 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'we the best Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "we the best ®️Bot",
+  botOwner: 'Silence Wilson',
+  ownerNumber: '27721305086', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
